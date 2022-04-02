1bba84354aab6eed01a4bd8aeadc77c6a9067dd3
blueishot <fabricioteixeirahungria@gmail.com>
2022-04-02 14:51:08 -0300

    Card changer (JS)

---
+++ js/index.js
@@ -0,0 +1,135 @@
+let title = window.document.getElementsByClassName('cardTitle')[0]
+let cardBackground = window.document.getElementsByClassName('card')[0]
+let image = window.document.getElementsByClassName('characterImage')[0]
+let race = window.document.getElementsByClassName('race')[0]
+let birth = window.document.getElementsByClassName('birth')[0]
+let gender = window.document.getElementsByClassName('gender')[0]
+let statu = window.document.getElementsByClassName('status')[0]
+
+function Roxy() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Roxy Migurdia"
+    cardBackground.style.backgroundColor = '#5e69a0'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/roxyanime.png'
+    race.innerHTML='Race: Migurd'
+    birth.innerHTML='Birth: K373'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Alive'
+}
+
+function Eris() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Eris Boreas Greyrat"
+    cardBackground.style.backgroundColor = '#ac4536'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/erisanime_6.png'
+    race.innerHTML='Race: Human'
+    birth.innerHTML='Birth: K405'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Alive'
+}
+
+function Sylphiette() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Sylphiette"
+    cardBackground.style.backgroundColor = '#82a759'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/sylphyanime_7.png'
+    race.innerHTML='Race: Elf / Human'
+    birth.innerHTML='Birth: K407'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Alive'
+}
+
+function Ruijerd() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Ruijerd Superdia"
+    cardBackground.style.backgroundColor = '#529758'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/ruijerd_anime.png'
+    race.innerHTML='Race: Demon (Supard)'
+    birth.innerHTML='Birth: Before K149'
+    gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
+
+function Ghislaine() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Ghislaine Dedoldia"
+    cardBackground.style.backgroundColor = '#b0a392'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/ghislaine_dedoldia_anime.png'
+    race.innerHTML='Race: Beast Race'
+    birth.innerHTML='Birth: ?'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Alive'
+}
+
+function Kishirika() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Kishirika Kishirisu"
+    cardBackground.style.backgroundColor = '#c14eba'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/kishirika_anime.png'
+    race.innerHTML='Race: Demon Race'
+    birth.innerHTML='Birth: K418'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Alive'
+}
+
+function Orsted() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Orsted"
+    cardBackground.style.backgroundColor = '#c6c6c5'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/orsted_anime.png'
+    race.innerHTML='Race: Dragon Race'
+    birth.innerHTML='Birth: ?'
+    gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
+
+function Rudeus() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Rudeus Greyrat"
+    cardBackground.style.backgroundColor = '#cb9a61'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/rudeus_anime_3.png'
+    race.innerHTML='Race: Human'
+    birth.innerHTML='Birth: K407'
+    gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
