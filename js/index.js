488fff764cf91498b089c0d9ec9af594077492c0
blueishot <fabricioteixeirahungria@gmail.com>
2022-04-02 17:19:56 -0300

    second row

--- js/index.js
+++ js/index.js
@@ -81,7 +81,7 @@ function Ghislaine() {
     cardBackground.style.backgroundColor = '#b0a392'
     image.src='https://static.tvtropes.org/pmwiki/pub/images/ghislaine_dedoldia_anime.png'
     race.innerHTML='Race: Beast Race'
-    birth.innerHTML='Birth: ?'
+    birth.innerHTML='Birth: Unknown'
     gender.innerHTML='Gender: Female'
     statu.innerHTML='Status: Alive'
 }
@@ -113,7 +113,7 @@ function Orsted() {
     cardBackground.style.backgroundColor = '#c6c6c5'
     image.src='https://static.tvtropes.org/pmwiki/pub/images/orsted_anime.png'
     race.innerHTML='Race: Dragon Race'
-    birth.innerHTML='Birth: ?'
+    birth.innerHTML='Birth: Unknown'
     gender.innerHTML='Gender: Male'
     statu.innerHTML='Status: Alive'
 }
@@ -131,5 +131,133 @@ function Rudeus() {
     race.innerHTML='Race: Human'
     birth.innerHTML='Birth: K407'
     gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
+
+function Paul() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Paul Greyrat"
+    cardBackground.style.backgroundColor = '#ac7d51'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/paulanime_4.png'
+    race.innerHTML='Race: Human'
+    birth.innerHTML='Birth: K388'
+    gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
+
+function Zenith() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Zenith Greyrat"
+    cardBackground.style.backgroundColor = '#e1b375'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/zenithanime.png'
+    race.innerHTML='Race: Human'
+    birth.innerHTML='Birth: K390'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Unknown'
+}
+
+function Lilia() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Lilia Greyrat"
+    cardBackground.style.backgroundColor = '#89474b'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/liliaanime_6.png'
+    race.innerHTML='Race: Human'
+    birth.innerHTML='Birth: K38K'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Alive'
+}
+
+function Elinalise() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Elinalise Dragonroad"
+    cardBackground.style.backgroundColor = '#e5b97a'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/elinalise_anime.png'
+    race.innerHTML='Race: Elf'
+    birth.innerHTML='Birth: Unknown'
+    gender.innerHTML='Gender: Female'
+    statu.innerHTML='Status: Alive'
+}
+
+function Talhand() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Talhand"
+    cardBackground.style.backgroundColor = '#b2783f'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/talhand_anime.png'
+    race.innerHTML='Race: Dwarf'
+    birth.innerHTML='Birth: K3XX'
+    gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
+
+function Geese() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Geese Nukadia"
+    cardBackground.style.backgroundColor = '#795944'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/geese_nukadia_anime.png'
+    race.innerHTML='Race: Demon Race'
+    birth.innerHTML='Birth: K3XX'
+    gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
+
+function Almanfi() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Almanfi"
+    cardBackground.style.backgroundColor = '#e7c174'
+    image.src='https://mushokutensei.jp/wp-content/themes/mushoku_re/img/character/img_character13.png'
+    race.innerHTML='Race: Spirit Familiar'
+    birth.innerHTML='Birth: Unknown'
+    gender.innerHTML='Gender: Male'
+    statu.innerHTML='Status: Alive'
+}
+
+function mysteryGirl() {
+    title.innerHTML = ''
+    image.src = ''
+    race.innerHTML=''
+    birth.innerHTML=''
+    gender.innerHTML=''
+    statu.innerHTML=''
+    title.innerHTML = "Nanahoshi Shizuka"
+    cardBackground.style.backgroundColor = '#242426'
+    image.src='https://static.tvtropes.org/pmwiki/pub/images/nanahoshi_mask.png'
+    race.innerHTML='Race: Human'
+    birth.innerHTML='Birth: K417'
+    gender.innerHTML='Gender: Female'
     statu.innerHTML='Status: Alive'
 }
